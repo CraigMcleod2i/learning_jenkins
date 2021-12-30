19c7212553077f972c9e49dfb46102b6ad3766d4
Craig McLeod <craig.mcleod@2itesting.com>
2021-12-30 12:39:59 +0000

    Some simple ui tests

---
+++ cypress/integration/jenkins_ui.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+
+describe('Run some arbitrary UI tests', () => {
+    
+    before(() => {
+        cy.visit('http://localhost:8081')
+    })
+
+    it('has an input', () => {
+        cy.get('#post-field').should('have.attr', 'type', 'text')
+    })
+    it('adds a new record via the input', () => {
+        cy.get('#post-field').type('12345')
+        cy.get('#main button').click().wait(100)
+        cy.get('p').last().contains('12345')
+    })
+    it('cleans up the database', () => {
+        cy.get('p').last().should('have.attr', 'value')
+            .then(res => {
+                cy.request('DELETE', 'http://localhost:3000/test/'+res)
+                cy.reload()
+                cy.get('p').last().should('not.have.attr', 'value', res)
+            })
+        
+    })
+
+})

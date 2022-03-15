60edfd25fb1d3df65668454d15351ee86034474f
Craig McLeod <craig.mcleod@2itesting.com>
2022-03-15 14:07:06 +0000

    Change ip in ui

--- cypress/integration/jenkins_ui.js
+++ cypress/integration/jenkins_ui.js
@@ -4,7 +4,7 @@
 describe('Run some arbitrary UI tests', () => {
     
     before(() => {
-        cy.visit('http://localhost:8081')
+        cy.visit('http://127.0.0.1:8081')
     })
 
     it('has an input', () => {
